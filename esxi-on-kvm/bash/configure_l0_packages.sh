cd8c8a43928cc4509af4add11d2be263ba33dfe1
seahow-uk <darmok.and.jalad@tanagra.uk>
2023-01-02 12:43:52 +0000

    test

--- esxi-on-kvm/bash/configure_l0_packages.sh
+++ esxi-on-kvm/bash/configure_l0_packages.sh
@@ -24,8 +24,6 @@
 	mkdir -p /mnt/iso /var/www/html OVA VM /etc/samba /var/log/pip
 
 ## level set
-	dnf config-manager --disable epel-next
-	dnf config-manager --disable epel
 	dnf config-manager --enable ha
 	dnf config-manager --enable powertools
 	dnf config-manager --enable nfv
@@ -34,7 +32,7 @@
 	rm -rfv /var/cache/dnf
 	dnf distro-sync -y
 	dnf update -y
-	
+
 	dnf install centos-release-openstack-xena -y
 	dnf install python39 -y
 	dnf install python2 -y
