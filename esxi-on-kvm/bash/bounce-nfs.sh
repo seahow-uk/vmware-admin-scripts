99b2a78b3cc513346b89e200b00b581f2d548260
seahow-uk <darmok.and.jalad@tanagra.uk>
2023-01-07 00:55:34 +0000

    added nfs bouncer

---
+++ esxi-on-kvm/bash/bounce-nfs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+#stop all the individual components
+systemctl stop nfs-server
+systemctl stop nfsdcld
+systemctl stop proc-fs-nfsd
+systemctl stop proc-fs-nfsd.mount
+systemctl stop var-lib-nfs-rpc_pipefs.mount
+systemctl stop nfs-client.target
+
+#restart them in the opposite order
+systemctl start nfs-client.target
+systemctl start var-lib-nfs-rpc_pipefs.mount
+systemctl start proc-fs-nfsd.mount
+systemctl start nfsdcld
+systemctl start nfs-server
