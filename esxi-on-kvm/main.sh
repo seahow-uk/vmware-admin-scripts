7cefded47d88e0ee89cf9a6d6ee6ac5487a54521
seahow-uk <darmok.and.jalad@tanagra.uk>
2023-01-16 14:47:53 +0000

    bumped hosts up to 16GB RAM for VSAN 8.0

--- esxi-on-kvm/main.sh
+++ esxi-on-kvm/main.sh
@@ -36,7 +36,7 @@ SSOPASSWORD=
 ##
 ## In total, the default will require the following physical resources on the server for the KVM domains
 ##
-## Memory: 148GB
+## Memory: 160GB
 ## Cores: 20 
 ## 
 ## [Due to the way I've got various services running on L0 you should leave a decent amount of cpu cores and ram free]
@@ -48,7 +48,7 @@ SSOPASSWORD=
 ## m5zn.metal is almost as cheap as c5n.metal at $3.96/hour or $2893.79/month (on demand)
 ##
 
-MEM=14
+MEM=16
 CORE=2
 MGMTMEM=18
 MGMTCORE=2
